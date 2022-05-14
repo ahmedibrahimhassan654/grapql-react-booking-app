e61ba93f49289bf400dffd38b9ec30762adac276
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-05-14 14:58:45 +0200

    5 GraphQL + MongoDB

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const Event = require("./models/event");
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 // Connect to database
 connectDB();
 const app = express();
 
-const events = [];
 app.use(bodyParser.json());
 app.use(
   "/graphql",
@@ -50,18 +50,31 @@ app.use(
          `),
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then((events) => {
+            return events.map((event) => {
+              return { ...event._doc, _id: event.id };
+            });
+          })
+          .catch((err) => {
+            throw err;
+          });
       },
       createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: args.eventInput.date,
-        };
-        events.push(event);
-        return event;
+        try {
+          const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: new Date(args.eventInput.date),
+          });
+          return event.save().then((result) => {
+            console.log(result);
+            return { ...result._doc, _id: result._doc._id.toString() };
+          });
+        } catch (err) {
+          throw err;
+        }
       },
     },
     graphiql: true,
