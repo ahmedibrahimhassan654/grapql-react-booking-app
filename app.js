c503474984167f726b49b6d3b785a8edfecc656f
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-05-14 03:18:23 +0200

    first qraphql api

--- app.js
+++ app.js
@@ -1,3 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 const app = express();
+
+app.use(bodyParser.json());
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+
+
+
+
+    
+    type RootQuery {
+        events: [String!]!
+    }
+    type RootMutation {
+        createEvent(name: String): String
+
+    }
+
+
+    schema {
+         query:RootQuery
+         mutation:RootMutation
+        }
+         `),
+    rootValue: {
+      events: () => {
+        return ["event1", "event2"];
+      },
+      createEvent: (args) => {
+        const eventName = args.name;
+        return eventName;
+      },
+    },
+    graphiql: true,
+  })
+);
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
